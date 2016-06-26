b8846804c863544eed6934bf811d3d39a7c4abe5
Albert Varaksin <albeva@me.com>
2016-06-26 16:23:17 +0100

    new clean setup

---
+++ lbc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lbc
+//
+//  Created by Albert on 26/06/2016.
+//  Copyright © 2016 LightBASIC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
